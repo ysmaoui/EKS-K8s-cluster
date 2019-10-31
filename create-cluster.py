613f85577fed34eff7d831ba32929c9a64a84d3e
Yacine Smaoui <yacine.smaoui@carmeq.com>
2019-10-31 17:26:33 +0100

    A working version that creates the network stack

--- create-cluster.py
+++ create-cluster.py
@@ -1,10 +1,12 @@
 import boto3
+import botocore
 import os
 
-CLUSTER_NAME = os.environ("CLUSTER_NAME")
-EKS_ROLE_ARN = os.environ("EKS_ROLE_ARN")
-WORKERS_SSH_KEY_NAME = os.environ("SSH_KEY_NAME")
-WORKERS_AMI_ID = os.environ("WORKERS_AMI")
+REGION = os.getenv("REGION")
+CLUSTER_NAME = os.getenv("CLUSTER_NAME")
+EKS_ROLE_ARN = os.getenv("EKS_ROLE_ARN")
+WORKERS_SSH_KEY_NAME = os.getenv("SSH_KEY_NAME")
+WORKERS_AMI_ID = os.getenv("WORKERS_AMI")
 
 cluster_network_parameters = [
     {
@@ -47,6 +49,46 @@ cluster_worker_nodes_parameters = [
     }
 ]
 
+cluster_network_template = os.path.join(".", "cluster_network.yml")
+
+cloudformation = boto3.client('cloudformation', region_name='us-west-2')
+
 
 def main():
-    cloudformation = boto3.client('cloudformation')
+
+    template_data = _parse_template(cluster_network_template)
+    parameter_data = cluster_network_parameters
+
+    params = {
+        'StackName': CLUSTER_NAME,
+        'TemplateBody': template_data,
+        'Parameters': parameter_data
+    }
+
+    try:
+        response = cloudformation.create_stack(**params)
+
+        print(response)
+
+        waiter = cloudformation.get_waiter('stack_create_complete')
+
+        print("...waiting for stack to be ready...")
+        waiter.wait(StackName=CLUSTER_NAME)
+    except botocore.exceptions.ClientError as ex:
+        error_message = ex.response['Error']['Message']
+        if error_message == 'No updates are to be performed.':
+            print("No changes")
+        else:
+            raise
+
+
+def _parse_template(template):
+    with open(template) as template_fileobj:
+        template_data = template_fileobj.read()
+        cloudformation.validate_template(TemplateBody=template_data)
+
+        return template_data
+
+
+if __name__ == '__main__':
+    main()
