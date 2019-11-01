126ad004662d95c186865135f0ae18b587fa4e69
Yacine Smaoui <yacine.smaoui@carmeq.com>
2019-11-01 12:41:53 +0100

    refactored create_or_update function

--- create-cluster.py
+++ create-cluster.py
@@ -1,12 +1,20 @@
 import boto3
 import botocore
 import os
+import json
+import sys
+
+from datetime import datetime
+
+NEEDED_ENVIRONMENT_VARIABLES = ['REGION', 'CLUSTER_NAME', 'EKS_ROLE_ARN', 'SSH_KEY_NAME',
+                                'WORKERS_AMI']
 
 REGION = os.getenv("REGION")
 CLUSTER_NAME = os.getenv("CLUSTER_NAME")
 EKS_ROLE_ARN = os.getenv("EKS_ROLE_ARN")
 WORKERS_SSH_KEY_NAME = os.getenv("SSH_KEY_NAME")
 WORKERS_AMI_ID = os.getenv("WORKERS_AMI")
+
 
 cluster_network_parameters = [
     {
@@ -56,30 +64,11 @@ cloudformation = boto3.client('cloudformation', region_name='us-west-2')
 
 def main():
 
-    template_data = _parse_template(cluster_network_template)
-    parameter_data = cluster_network_parameters
+    check_environment_variables()
 
-    params = {
-        'StackName': CLUSTER_NAME,
-        'TemplateBody': template_data,
-        'Parameters': parameter_data
-    }
-
-    try:
-        response = cloudformation.create_stack(**params)
-
-        print(response)
-
-        waiter = cloudformation.get_waiter('stack_create_complete')
-
-        print("...waiting for stack to be ready...")
-        waiter.wait(StackName=CLUSTER_NAME)
-    except botocore.exceptions.ClientError as ex:
-        error_message = ex.response['Error']['Message']
-        if error_message == 'No updates are to be performed.':
-            print("No changes")
-        else:
-            raise
+    print(cluster_network_parameters)
+    create_or_update_stack("K8S-cluster-network", cluster_network_template,
+                           cluster_network_parameters)
 
 
 def _parse_template(template):
@@ -88,6 +77,73 @@ def _parse_template(template):
         cloudformation.validate_template(TemplateBody=template_data)
 
         return template_data
+
+
+def _stack_exists(stack_name):
+    stacks = cloudformation.list_stacks()['StackSummaries']
+    for stack in stacks:
+        if stack['StackStatus'] == 'DELETE_COMPLETE':
+            continue
+        if stack_name == stack['StackName']:
+            return True
+    return False
+
+
+def create_or_update_stack(stack_name, template, parameters):
+    template_data = _parse_template(template)
+    parameter_data = parameters
+
+    params = {
+        'StackName': stack_name,
+        'TemplateBody': template_data,
+        'Parameters': parameter_data,
+    }
+    print("PARAMETERS")
+    print(parameter_data)
+    try:
+        if _stack_exists(stack_name):
+            print('Updating {}'.format(stack_name))
+            stack_result = cloudformation.update_stack(**params)
+            waiter = cloudformation.get_waiter('stack_update_complete')
+        else:
+            print('Creating {}'.format(stack_name))
+            stack_result = cloudformation.create_stack(**params)
+            waiter = cloudformation.get_waiter('stack_create_complete')
+        print("...waiting for stack to be ready...")
+        waiter.wait(StackName=stack_name)
+    except botocore.exceptions.ClientError as ex:
+        error_message = ex.response['Error']['Message']
+        if error_message == 'No updates are to be performed.':
+            print("No changes")
+        else:
+            raise
+    else:
+        print(json.dumps(
+            cloudformation.describe_stacks(StackName=stack_result['StackId']),
+            indent=2,
+            default=json_serial
+        ))
+
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError("Type not serializable")
+
+
+def check_environment_variables():
+    passed = True
+    for env in NEEDED_ENVIRONMENT_VARIABLES:
+        if env in os.environ:
+            pass
+        else:
+            print('Please set the environment variable: {}'.format(env))
+            passed = False
+
+    if not passed:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
